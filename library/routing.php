a6f05ab2d97d102a8745849052048f4e61378879
ravduda <ravduda@gmail.com>
2023-05-23 00:21:58 +0200

    Added routes

--- library/routing.php
+++ library/routing.php
@@ -7,5 +7,18 @@ App::getRouter()->setDefaultRoute('hello'); #default action
 //App::getRouter()->setLoginRoute('login'); #action to forward if no permissions
 
 Utils::addRoute('login', 'LoginCtrl');
-Utils::addRoute('hello', 'HelloCtrl');
+Utils::addRoute('logout', 'LogoutCtrl');
+Utils::addRoute('users', 'UserCtrl');
+Utils::addRoute('addUser', 'UserCtrl');
+Utils::addRoute('rmUser', 'UserCtrl');
+Utils::addRoute('authors', 'AuthorCtrl');
+Utils::addRoute('addAuthor', 'AuthorCtrl');
+Utils::addRoute('rmAuthor', 'AuthorCtrl');
+Utils::addRoute('titles', 'TitleCtrl');
+Utils::addRoute('addTitle', 'TitleCtrl');
+Utils::addRoute('rmTitle', 'TitleCtrl');
+Utils::addRoute('books', 'BookCtrl');
+Utils::addRoute('addBook', 'BookCtrl');
+Utils::addRoute('rmBook', 'BookCtrl');
+
 //Utils::addRoute('action_name', 'controller_class_name');
