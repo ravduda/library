4f7e87d6420688ff9627592c9a7c17a364b789c6
ravduda <ravduda@gmail.com>
2023-05-23 00:27:57 +0200

    Updated routing

--- library/routing.php
+++ library/routing.php
@@ -3,22 +3,26 @@
 use core\App;
 use core\Utils;
 
-App::getRouter()->setDefaultRoute('hello'); #default action
+App::getRouter()->setDefaultRoute('titles'); #default action
 //App::getRouter()->setLoginRoute('login'); #action to forward if no permissions
 
 Utils::addRoute('login', 'LoginCtrl');
-Utils::addRoute('logout', 'LogoutCtrl');
-Utils::addRoute('users', 'UserCtrl');
-Utils::addRoute('addUser', 'UserCtrl');
-Utils::addRoute('rmUser', 'UserCtrl');
-Utils::addRoute('authors', 'AuthorCtrl');
-Utils::addRoute('addAuthor', 'AuthorCtrl');
-Utils::addRoute('rmAuthor', 'AuthorCtrl');
-Utils::addRoute('titles', 'TitleCtrl');
-Utils::addRoute('addTitle', 'TitleCtrl');
-Utils::addRoute('rmTitle', 'TitleCtrl');
-Utils::addRoute('books', 'BookCtrl');
-Utils::addRoute('addBook', 'BookCtrl');
-Utils::addRoute('rmBook', 'BookCtrl');
+// Utils::addRoute('logout', 'LogoutCtrl', ['user', 'admin']);
 
-//Utils::addRoute('action_name', 'controller_class_name');
+// Utils::addRoute('users', 'UserCtrl', ['admin']);
+// Utils::addRoute('addUser', 'UserCtrl', ['admin']);
+// Utils::addRoute('rmUser', 'UserCtrl', ['admin']);
+
+// Utils::addRoute('authors', 'AuthorCtrl');
+// Utils::addRoute('addAuthor', 'AuthorCtrl', ['admin']);
+// Utils::addRoute('rmAuthor', 'AuthorCtrl', ['admin']);
+
+// Utils::addRoute('titles', 'TitleCtrl');
+// Utils::addRoute('addTitle', 'TitleCtrl', ['admin']);
+// Utils::addRoute('rmTitle', 'TitleCtrl', ['admin']);
+
+// Utils::addRoute('books', 'BookCtrl');
+// Utils::addRoute('addBook', 'BookCtrl', ['admin']);
+// Utils::addRoute('rmBook', 'BookCtrl', ['admin']);
+
+// Utils::addRoute('extend', 'BookCtrl', ['user']);
