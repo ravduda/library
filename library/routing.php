ffd68710ba5b04b84178a7b5545351649f9ac7ff
ravduda <ravduda@gmail.com>
2023-05-26 11:17:39 +0200

    Changed bootstrap for tailwind

--- library/routing.php
+++ library/routing.php
@@ -3,7 +3,7 @@
 use core\App;
 use core\Utils;
 
-App::getRouter()->setDefaultRoute('titles'); #default action
+App::getRouter()->setDefaultRoute('login'); #default action
 //App::getRouter()->setLoginRoute('login'); #action to forward if no permissions
 
 Utils::addRoute('login', 'LoginCtrl');
