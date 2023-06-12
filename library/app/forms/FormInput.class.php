3b913f6d542281077382ba7dc9384c49e531d2b5
ravduda <ravduda@gmail.com>
2023-06-13 00:53:07 +0200

    Added constructor and validator to form input

--- library/app/forms/FormInput.class.php
+++ library/app/forms/FormInput.class.php
@@ -3,10 +3,23 @@
 namespace app\forms;
 
 class FormInput {
-    public $type;
     public $name;
+    public $type;
     public $label;
+    public $validationRules;
     public $options;
 
     public $value;
+
+    public function __construct($name, $type, $label, $validationRules, $options=[]){
+        $this->name = $name;
+        $this->type = $type;
+        $this->label = $label;
+        $this->validationRules = $validationRules;
+        $this->options = $options;
+    }
+
+    public getAndValidate(){
+        $this->value = $v->validateFromPost($name, $validationRules);
+    }
 }
